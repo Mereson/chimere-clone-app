173cecaba668b2fc77e5b1c0a7f9b3a30951773e
Mereson <chimeremnmaojinta@gmail.com>
2024-04-29 19:47:33 +0100

    beginning stages

---
+++ src/Pages/Userpage.js
@@ -0,0 +1,12 @@
+import React from "react"
+import "../input.css"
+
+const Userpage = () => {
+  return (
+    <>
+      <p className=" text-purple-700">Netflix Site</p>
+    </>
+  )
+}
+
+export default Userpage
